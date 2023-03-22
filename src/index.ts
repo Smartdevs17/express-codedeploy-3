5f2e811c47e27c3e5fc558600c7b1526d4cda654
Tech Support <tech@mycantant.com>
2023-03-22 12:45:26 +0100

    update text

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req: Request, res: Response) => {
-    res.send("Hello World, Victor want teach me english")
+    res.send("Hello World, Devsops the carry me where i no know")
 })
 
 app.get('/health', (req: Request, res: Response) => {
